eb5b04751600bfdda7be68b5db1ed266ae2efc9c
saitou-hiroaki <3110hiroai0410@gmail.com>
2020-10-07 14:24:20 +0900

    リファクタリング作業その１ 　・クラス名の変更 　・クラスの宣言と定義部分のファイルに分割 　・共通でヘッターファイルをCommon,ｈに移動

---
+++ ClassPractice/ClassPractice/Common.h
@@ -0,0 +1,12 @@
+
+#ifndef COMMON_H
+#define COMMON_H
+
+//今まで書いてきた#include<>は
+//システム標準の場所にあるファイルをインクルードする時に使う
+#include <stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+#endif //#ifndef COMMON_H
+
---
+++ Common.h
@@ -0,0 +1,16 @@
+
+#ifndef COMMON_H
+#define COMMON_H
+
+//今まで書いてきた#include<>は
+//システム標準の場所にあるファイルをインクルードする時に使う
+#include <stdlib.h>
+#include<stdio.h>
+#include<time.h>
+
+const int MAP_X = 11;   //マップの横
+const int MAP_Y = 13;   //マップの縦
+
+
+#endif //#ifndef COMMON_H
+
