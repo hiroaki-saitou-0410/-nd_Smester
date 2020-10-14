18de8607d3b087609121525908e28a86f561af17
saitou-hiroaki <3110hiroai0410@gmail.com>
2020-10-14 16:43:16 +0900

    継承・汎化に関する実習用プロジェクトの作成

--- 02_1014/Practice02/practice02_03_iterator/practice02_03_iterator.cpp
+++ 02_1014/Practice02/practice02_03_iterator/practice02_03_iterator.cpp
@@ -9,12 +9,13 @@ int main()
 {
 	srand((unsigned)time(NULL));
 
-	std::vector<int> v1(8);				//要素8、すべて0で初期化
+	//要素8、すべて0で初期化
+	std::vector<int> v1(8);	
 	
 	//iteratorを使ったfor文の回し方
 	//for (int i = 0; i < v1.size(); i++)
 	//型:std::vector<int>::iterator
-	std::vector<int>::iterator itr;
+	std::vector<int>::iterator itr;		//iteratorは反復子
 	for (itr=v1.begin();itr !=v1.end();++itr)
 	{
 		*itr = rand();
---
+++ 02_1014/Practice02/practice02_04/practice02_04.cpp
@@ -0,0 +1,11 @@
+﻿
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+	system("pause");
+	return 0;
+}
